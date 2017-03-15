3ab85344fae93eaea0a5b5c92894929641c852b3
WiLdCaT F8n32 <jeanpierrenjockii@utexas.edu>
2017-03-15 09:41:28 -0500

    Twitch api python script

---
+++ twitch.py
@@ -0,0 +1,15 @@
+import requests
+
+def main():
+
+	#r = requests.get('https://api.twitch.tv/kraken/users/Heinz?client_id=e6xu67x7c493rmp1osdcrnivd3j8g3')
+	#print(r.json())
+
+	
+	streams = requests.get('https://api.twitch.tv/kraken/streams/?game=Halo 5: Guardians&client_id=e6xu67x7c493rmp1osdcrnivd3j8g3')
+	stream_json = streams.json()
+
+
+	stream_data = stream_json["streams"]
+	print(stream_data[1])
+main()
