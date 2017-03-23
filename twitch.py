3423b20a4532cd0d4e63fa5a1930564120709503
WiLdCaT F8n32 <jeanpierrenjockii@utexas.edu>
2017-03-22 20:41:31 -0500

    edits to about page

--- twitch.py
+++ twitch.py
@@ -6,12 +6,9 @@ def main():
 	#print(r.json())
 
 	
-	streams = requests.get('https://api.twitch.tv/kraken/streams/?game=The Long Dark&client_id=e6xu67x7c493rmp1osdcrnivd3j8g3')
+	streams = requests.get('https://api.twitch.tv/kraken/channels/pokimane?client_id=e6xu67x7c493rmp1osdcrnivd3j8g3')
 	stream_json = streams.json()
 
 
-	stream_data = stream_json["streams"]
-	
-	print(stream_data[0])
-
+	print(stream_json)
 main()
